0bd82eecad5a4de2a0d1877cc05469f9041a0eff
crownow <p.denezhko@madcurve.com>
2023-09-27 11:06:31 +0300

    New

--- frontend/tad_app/src/registration/registrationForm.js
+++ frontend/tad_app/src/registration/registrationForm.js
@@ -1,143 +1,124 @@
-import React, { Component } from 'react';
+import React, {useState,setState} from 'react';
 import './style.css'
-import 'index.html'
-class RegistrationForm extends Component {
-  constructor() {
-    super();
-    this.state = {
-      name: '',
-      age: '',
-      sex: 'male',
-      orientation: 'straight',
-      city: 'unknown',
-      bio: '',
-      profilePhoto: null,
-    };
-  }
+function RegistrationForm() {
+    
+    const [Name, setName] = useState(null);
+    const [Age, setAge] = useState(null);
+    const [Gender, setGender] = useState(null);
+    const [Orientation,setOrientation] = useState(null);
+    const [City,setCity] = useState(null);
+    const [Bio,setBio] = useState(null);
 
-  handleInputChange = (e) => {
-    const { name, value } = e.target;
-    this.setState({ [name]: value });
-  }
+    function Dropdown() {
+        const [isOpen, setIsOpen] = useState(false);
+        const [selectedOption, setSelectedOption] = useState(null);
+      
+        const toggleDropdown = () => {
+          setIsOpen(!isOpen);
+        };
+      
+        const handleSelect = (option) => {
+          setSelectedOption(option);
+          setIsOpen(false); 
+        };
+    }
+    const handleInputChange = (e) => {
+        const {id , value} = e.target;
+        if(id === "Name"){
+            setName(value);
+        }
+        if(id === "Age"){
+            setAge(value);
+        }
+        if(id === "Gender"){
+            setGender(value);
+        }
+        if(id === "Orientation"){
+            setOrientation(value);
+        }
+        if(id === "City"){
+            setCity(value);
+        }
+        if(id === "Bio"){
+            setBio(value);
+        }
 
-  handleFileChange = (e) => {
-    const file = e.target.files[0];
-    this.setState({ profilePhoto: file });
-  }
+    }
 
-  handleSubmit = (e) => {
-    e.preventDefault();
-    // Send to server
     const handleSubmit  = () => {
-        console.log(Name,lastName,email,password,confirmPassword);
+        console.log(Name,Age,Gender,Orientation,City,Bio);
     }
 
     return(
-        <div class="container">
-        <div class="row main-form">
-        <form class="" method="post" action="#">
+            <div class="limiter">
+                <div class="container-login100">
+                    <div class="wrap-login100">
+                        <form class="login100-form validate-form">
+                            <span class="login100-form-title p-b-26">
+                                Welcome
+                            </span>
+                            <span class="login100-form-title p-b-48">
+                                <i class="zmdi zmdi-font"></i>
+                            </span>
         
-        <div class="form-group">
-        <label for="name" class="cols-sm-2 control-label">Your Name</label>
-        <div class="cols-sm-10">
-        <div class="input-group">
-        <span class="input-group-addon"><i class="fa fa-user fa" aria-hidden="true"></i></span>
-        <input type="text" class="form-control" name="name" id="name" placeholder="Enter your Name"/>
-        </div>
-        </div>
-        </div>
-       
-        <div class="form-group">
-        <label for="age" class="cols-sm-2 control-label">Your Age</label>
-        <div class="cols-sm-10">
-        <div class="input-group">
-        <span class="input-group-addon"><i class="fa fa-envelope fa" aria-hidden="true"></i></span>
-        <input type="number" class="form-control" name="age" id="age" placeholder="Enter your Age"/>
-        </div>
-        </div>
-        </div>
-       
-        <div class="form-group">
-        <label for="sex" class="cols-sm-2 control-label">Sex
-        </label>
-        <div class="cols-sm-10">
-        <div class="input-group">
-        <span class="input-group-addon"><i class="fa fa-users fa" aria-hidden="true"></i></span>
-        <select type="sex" class="form-control" name="sex" id="sex" placeholder="Select your Sex"
-                     name0="sex"
-                     value={this.state.sex}
-                     onChange={this.handleInputChange}
-                   >
-                     <option value="male">Man</option>
-                     <option value="female">Women</option>
-                     <option value="Gay">Vertosexual</option>
-                     <option value="Gay">Doublesexual</option>
-                     <option value="other">Other</option>
-                   </select>
-       
-        </div>
-        </div>
-        </div>
-       
-        <div class="form-group">
-        <label for="orientation" class="cols-sm-2 control-label">Orientation
-        </label>
-        <div class="cols-sm-10">
-        <div class="input-group">
-        <span class="input-group-addon"><i class="fa fa-users fa" aria-hidden="true"></i></span>
-        <select type="orientation" class="form-control" name="orientation" id="orientation" placeholder="Select your Orientation"
-                     name1="orientation"
-                     value={this.state.orientation}
-                     onChange={this.handleInputChange}
-                   >
-                     <option value="Hetero">Hetero</option>
-                     <option value="Bi">Bi</option>
-                     <option value="Bi">Gay</option>
-                     <option value="Bi">Lesbi</option>
-                   </select>
-       
-        </div>
-        </div>
-        </div>
-       
-        <div class="form-group">
-        <label for="city" class="cols-sm-2 control-label">City
-        </label>
-        <div class="cols-sm-10">
-        <div class="input-group">
-        <span class="input-group-addon"><i class="fa fa-users fa" aria-hidden="false"></i></span>
-        <select type="city" class="form-control" name="ciry" id="ciry" placeholder="Select your Citizen"
-                     name2="sex"
-                     value={this.state.city}
-                     onChange={this.handleInputChange}
-                   >
-                     <option value="male">New Orsk</option>
-                     <option value="female">Chechnya</option>
-                     <option value="other">Maskvabad</option>
-                   </select>
-       
-        </div>
-        </div>
-        </div>
+                            <div class="wrap-input100 validate-input" data-validate = "Valid Name is: abc">
+                                <input class="input100" type="text" value={Name} name="Name"></input>
+                                <span class="focus-input100" data-placeholder="Name"></span>
+                            </div>
         
-        <div class="form-group">
-        <label for="bio" class="cols-sm-2 control-label">Bio</label>
-        <div class="cols-sm-10">
-        <div class="input-group">
-        <span class="input-group-addon"><i class="fa fa-user fa" aria-hidden="true"></i></span>
-        <input type="text" class="form-control" name="bio" id="bio" placeholder="Enter your Bio"/>
-        </div>
-        </div>
-        </div>
-       
-        <div class="form-group ">
-        <a href="#" target="_blank" type="button" id="button" class="btn btn-primary btn-lg btn-block login-button">Start Search</a>
-        </div>
+                            <div class="wrap-input100 validate-input" data-validate=">=0">
+                                <span class="btn-show-pass">
+                                    <i class="zmdi zmdi-eye"></i>
+                                </span>
+                                <input class="input100" type="number" value={Age} name="Age"></input>
+                                <span class="focus-input100" data-placeholder="Age"></span>
+                            </div>
+
+                            <div class="wrap-input100 validate-input">
+                                <span class="focus-input100"></span>
+                                    <select class="input100" name='Gender' id="Gender" value={Gender} onChange={handleInputChange} required>
+                                        <option value="Bi">Man</option>
+                                        <option value="Gay">Woman</option>
+                                        <option value="Hetero">DoubleSexual</option>
+                                    </select>
+                            </div>
+
+                            <div class="wrap-input100 validate-input" data-validate = "Valid Name is: abc">
+                                <span class="focus-input100"></span>
+                                    <select class="input100" name='Orientation' id="Orintation" value={Orientation} onChange={handleInputChange} required>
+                                        <option value="Bi">Bi</option>
+                                        <option value="Gay">Gay</option>
+                                        <option value="Hetero">Hetero</option>
+                                    </select>
+                            </div>
+
+                            <div class="wrap-input100 validate-input">
+                                <span class="focus-input100"></span>
+                                    <select class="input100" name='City' id="City" value={City} onChange={handleInputChange} required>
+                                        <option value="Bi">Moscow</option>
+                                        <option value="Gay">New York</option>
+                                        <option value="Hetero">Omsk</option>
+                                    </select>
+                            </div>
+
+                            <div class="wrap-input100 validate-input">
+                                <input class="input100" type="text" value={Bio} name="Bio"></input>
+                                <span class="focus-input100" data-placeholder="Bio"></span>
+                            </div>
         
-        </form>
-        </div>
-        </div>       
+                            <div class="container-login100-form-btn">
+                                <div class="wrap-login100-form-btn">
+                                    <div class="login100-form-bgbtn"></div>
+                                    <button class="login100-form-btn">
+                                        Start Search
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
     )       
 }
-}
-export default RegistrationForm;
+
+export default RegistrationForm
