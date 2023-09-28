f9ce2da1e387005a03befbe65d7821475c9cb827
crownow <p.denezhko@madcurve.com>
2023-09-28 22:58:25 +0300

    next fix

--- frontend/tad_app/src/registration/header.js
+++ frontend/tad_app/src/registration/header.js
@@ -1,5 +1,15 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 function Header() {
+    const [telegramId, setTelegramId] = useState('');
+
+    const extractTelegramData = () => {
+        const initData = window.TelegramWebApp?.initData;
+      
+        if (initData) {
+          const telegramId = initData.telegramId;
+          setTelegramId(telegramId);
+        }
+      };
     // return(
     //     // <nav class="bg-dark navbar-dark navbar">
     //     //     <div className="row col-12 d-flex justify-content-center text-white">
--- frontend/tad_app/src/registration/registrationForm.js
+++ frontend/tad_app/src/registration/registrationForm.js
@@ -1,5 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './style.css';
+import { authToken } from './header.js';
+
+
 function RegistrationPage() {
   const [name, setName] = useState('');
   const [age, setAge] = useState('');
@@ -10,68 +13,156 @@ function RegistrationPage() {
   const [error, setError] = useState('');
   const [selectedFile, setSelectedFile] = useState(null);
 
-  const validateName = (inputName) => {
-    if (/^[a-zA-Z]+$/.test(inputName) && inputName.length <= 20) {
-      setError('');
-      return true;
-    } else {
-      setError('Name only words');
-      return false;
-    }
-  };
+  const [telegramId, setTelegramId] = useState('');
+  const [photoUrl, setPhotoUrl] = useState('');
 
-  const validateBio = (inputBio) => {
-    if (inputBio.length <= 50) {
-          setError(""); 
-        } else {
-          setError("Bio >= 50 simbols");
-          return false
-        }
+
+  const [isDarkTheme, setIsDarkTheme] = useState(false); // Check dark theme
+
+  useEffect(() => {
+    // Check dark theme is active
+    if (
+      window.matchMedia &&
+      window.matchMedia('(prefers-color-scheme: dark)').matches
+    ) {
+      setIsDarkTheme(true);
+    }
+  }, []);
+
+  const validateName = (inputName) => {
+    const trimmedName = inputName.trim();
+    const nameRegex = /^[A-Za-z\s]+$/;
+    if (!nameRegex.test(trimmedName)) {
+        setError('Name should contain only letters and spaces.');
+        return false;
       }
-  
+      
+      if (trimmedName.length > 20) {
+        setError('Name should not exceed 20 characters.');
+        return false;
+      }
+      
+      // Если имя прошло валидацию
+      setError('');
+      setName(trimmedName); // Устанавливаем очищенное значение имени
+      return true;
+    };
 
   const validateAge = (inputAge) => {
     if (/^\d+$/.test(inputAge) && inputAge.length <= 2) {
       setError('');
       return true;
     } else {
-      setError('Age only alpha-numeric and <100');
+      setError('Age must consist of numbers only and no more than 2 characters');
       return false;
     }
   };
+  
+  const extractTelegramData = () => {
+    const initData = window.TelegramWebApp?.initData;
+
+    if (initData) {
+      const telegramId = initData.telegramId;
+      setTelegramId(telegramId);
+    }
+  };
+
+  useEffect(() => {
+    extractTelegramData();
+  }, []);
 
   const handleFileChange = (e) => {
     const file = e.target.files[0];
     setSelectedFile(file);
   };
 
+//   const handleSubmit = (e) => {
+//     e.preventDefault();
+//     if (!selectedFile) {
+//       setError('Please upload your photo');
+//       return;
+//     }
+//     if (validateName(name) && validateAge(age) && !error) {
+//       console.log('Data sent to server:', name, age, gender, orientation, city, bio);
+//       if (selectedFile) {
+//         console.log('Upload photo:', selectedFile);
+//       }
+//     }
+//   };
+
+const uploadPhotoAndGetUrl = () => {
+    const formData = new FormData();
+    formData.append('photo', selectedFile);
+
+    fetch('uploadphoto/api/228', {
+      method: 'POST',
+      body: formData,
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data && data.photoUrl) {
+          setPhotoUrl(data.photoUrl);
+        }
+      })
+      .catch((error) => {
+        console.error('error upload:', error);
+      });
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (validateName(name) && validateAge(age) && validateBio(bio)) {
-      console.log('Data send to server:', name, age, gender, orientation, city, bio);
-      if (selectedFile) {
-        console.log('Upload file:', selectedFile);
+    if (validateName(name) && validateAge(age) && !error) {
+      if (photoUrl) {
+        const userData = {
+          name: name,
+          age: age,
+          gender: gender,
+          orientation: orientation,
+          city: city,
+          bio: bio,
+          telegramId: telegramId, 
+          photoUrl: photoUrl, 
+        };
+
+        fetch('register/api/228', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(userData),
+        })
+          .then((response) => {
+            if (response.status === 200) {
+              console.log('200');
+            } else {
+              console.error('400');
+            }
+          })
+          .catch((error) => {
+            console.error('error:', error);
+          });
+      } else {
+        setError('500');
       }
     }
   };
 
   return (
-    <div className="registration-page">
+    <div className={`registration-page ${isDarkTheme ? 'dark-theme' : ''}`}>
+      {/* <div className="back-button">
+        <button>Back</button>
+      </div> */}
       <h1>Welcome</h1>
-      <div className="form-group-center">
-          <button type="submit">
-          <label htmlFor="file" className="upload-button">
-            <p className='start-search-button' type='submit'>Choose Your Photo</p>
-        </label>
-      <input
-        type="file"
-        id="file"
-        accept="image/*"
-        style={{ display: 'none' }}
-        onChange={handleFileChange}
-      />
-          </button>
-          </div>
+      <label htmlFor="file" className="upload-button">
+        Choose Your Photo
+        <input
+          type="file"
+          id="file"
+          accept="image/*"
+          style={{ display: 'none' }}
+          onChange={handleFileChange}
+        />
+      </label>
       <div className="image-preview">
         {selectedFile ? (
           <img src={URL.createObjectURL(selectedFile)} alt="Profile" />
@@ -80,94 +171,73 @@ function RegistrationPage() {
         )}
       </div>
       <form onSubmit={handleSubmit}>
-      <div className="form-group">
-          <label htmlFor="name">Name:</label>
+        <div className="form-group">
+          <label htmlFor="Name">Name</label>
           <input
             type="text"
-            id="name"
+            id="Name"
             value={name}
-            placeholder='Enter you Name'
             onChange={(e) => setName(e.target.value)}
-            required
           />
         </div>
         <div className="form-group">
-          <label htmlFor="age">Age:</label>
+          <label htmlFor="Age">Age</label>
           <input
-            type="text"
-            id="age"
+            type="number"
+            id="Age"
             value={age}
-            placeholder='Enter your Age'
             onChange={(e) => setAge(e.target.value)}
-            required
           />
         </div>
+        <script src="https://telegram.org/js/telegram-web-app.js"></script>
         <div className="form-group">
-          <label htmlFor="gender">Gender:</label>
-          <select
-            id="gender"
-            value={gender}
-            onChange={(e) => setGender(e.target.value)}
-            required
-          >
-            <option value="">Select Gender</option>
+          <label htmlFor="Gender">Gender</label>
+          <select id="Gender" value={gender} onChange={(e) => setGender(e.target.value)}>
             <option value="Male">Male</option>
             <option value="Female">Female</option>
-            {/* <option value="Non-Binary">Non-Binary</option>
-            <option value="Transgender">Transgender</option> */}
+            {/* <option value="Other">Other</option>
+            <option value="Prefer not to say">Prefer not to say</option>
+            <option value="Non-binary">Non-binary</option> */}
+          </select>
+        </div>
+        <div className="form-group">
+          <label htmlFor="Orientation">Orientation</label>
+          <select
+            id="Orientation"
+            value={orientation}
+            onChange={(e) => setOrientation(e.target.value)}
+          >
+            <option value="Straight">Straight</option>
+            <option value="Gay">Gay</option>
+            {/* <option value="Lesbian">Lesbian</option> */}
+            <option value="Bisexual">Bisexual</option>
             {/* <option value="Other">Other</option> */}
           </select>
         </div>
         <div className="form-group">
-          <label htmlFor="orientation">Orientation:</label>
-          <select
-            id="orientation"
-            value={orientation}
-            onChange={(e) => setOrientation(e.target.value)}
-            required
-          >
-            <option value="">Select Orientation</option>
-            <option value="Heterosexual">Straight</option>
-            <option value="Homosexual">Bi</option>
-            <option value="Bisexual">Gay</option>
-            {/* <option value="Heterosexual">Heterosexual</option>
-            <option value="Homosexual">Homosexual</option>
-            <option value="Bisexual">Bisexual</option>
-            <option value="Pansexual">Pansexual</option>
-            <option value="Asexual">Asexual</option> */}
+          <label htmlFor="City">City</label>
+          <select id="City" value={city} onChange={(e) => setCity(e.target.value)}>
+            <option value="New York">New York</option>
+            <option value="Los Angeles">Moscow</option>
+            <option value="Chicago">Rome</option>
+            {/* <option value="San Francisco">San Francisco</option>
+            <option value="Miami">Miami</option> */}
           </select>
         </div>
         <div className="form-group">
-          <label htmlFor="city">City:</label>
-          <select
-            id="city"
-            value={city}
-            onChange={(e) => setCity(e.target.value)}
-            required
-          >
-            <option value="">Select City</option>
-            <option value="New York">Moscow</option>
-            <option value="Los Angeles">Rome</option>
-            <option value="London">New York</option>
-            {/* <option value="New York">New York</option>
-            <option value="Los Angeles">Los Angeles</option>
-            <option value="London">London</option>
-            <option value="Paris">Paris</option>
-            <option value="Tokyo">Tokyo</option> */}
-          </select>
-        </div>
-        <div className="form-group">
-          <label htmlFor="bio">Bio:</label>
+          <label htmlFor="Bio">Bio</label>
           <textarea
-            id="bio"
-            value={bio}
+            id="Bio"
             placeholder='Enter your Bio'
+            value={bio}
             onChange={(e) => setBio(e.target.value)}
-          ></textarea>
+          />
         </div>
         {error && <p className="error">{error}</p>}
-        <div className="form-group-center">
-          <button type="submit">Start Search</button>
+        <div className="form-group center">
+          <button type="submit" className="start-search-button">
+            Start Search
+          </button>
         </div>
       </form>
     </div>
