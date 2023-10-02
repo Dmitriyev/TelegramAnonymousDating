03f78f38a98113d1845a2885673a64626c48a727
crownow <p.denezhko@madcurve.com>
2023-10-03 00:26:19 +0300

    New Desing

--- frontend/tad_app/src/App.js
+++ frontend/tad_app/src/App.js
@@ -1,16 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
-import Header from './registration/header';
-import RegistrationForm from './registration/registrationForm'
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./registration/header";
+import RegistrationForm from "./registration/registrationForm";
+import Menu from "./menu/menuForm";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <RegistrationForm/>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/start" element={<RegistrationForm />}/>
+        <Route path="/menu" element={<Menu />} />
+          {/* <Route path="blogs" element={<Blogs />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="*" element={<NoPage />} /> */}
+      </Routes>
+    </Router>
+      <Header />
+      {/* <RegistrationForm /> */}
+      </div>
   );
 }
 
 export default App;
-
